098ceeb1df143557083fbe60bbe011ecb8748eda
Moh <mohammad4sure@gmail.com>
2022-08-26 09:52:11 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,92 @@
-const WebSocket = require('ws')
-const XMPP = require("stanza") 
+const { Client, IntentsBitField } = require('discord.js');
+const XMPP = require("stanza")
 const crypto = require("crypto")
-const ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
-require("dotenv").config();
-      const payload = {
-      	op: 2,
-      	d: {
-      		token: process.env.BOT_TOKEN,
-      		properties: {
-      			$os: "linux",
-      			$browser: "chrome",
-      			$device: "chrome",
-      		},
-      	},
-      };
-      ws.on("open", () => {
-      	ws.send(JSON.stringify(payload));
-      });
-      ws.on("message", (data) => {
-      	let payload = JSON.parse(data);
-      	const { t, event, op, d	} = payload;
-      	switch(op) {
-      		case 10:
-      			const {	heartbeat_interval } = d;
-      			interval = heartbeat(heartbeat_interval);
-      			console.log("Websocket Has Started");
-      			break;
-      	}
-      	switch(t) {
-      		case "PRESENCE_UPDATE":
-      			if(!d.user.id === process.env.DISCORD_ID) return;
-      				let onlineType = d.status
-      				let status = d.game.state
-      				if(d.status === "idle") onlineType == "away";
-      				if(!d?.game) status == " ";
-      				const client = XMPP.createClient({
-      					jid:process.env.FORTNITE_ID + "@prod.ol.epicgames.com",
-      					transports: {
-      						websocket: `wss://xmpp-service-prod.ol.epicgames.com`,
-      						bosh: false,
-      					},
-      					credentials: {
-      						host: "prod.ol.epicgames.com",
-      						username: process.env.FORTNITE_ID,
-      						password: process.env.ACCESS_TOKEN,
-      					},
-      					resource: `V2:Fortnite:PC::${crypto.randomBytes(16).toString('hex').toUpperCase()}`
-      				});
-      				client.on('session:started', () => {
-      					client.getRoster();
-      					client.sendPresence({
-      						status: status,
-      						onlineType: onlineType, 
-                                          bIsPlaying: true, 
-                                          ProductName: "Fortnite"
-      					})
-      				})
-                              client.connect();
-      	}
-      });
-      const heartbeat = (ms) => {
-      	return setInterval(() => {
-      		ws.send(JSON.stringify({
-      			op: 1,
-      			d: null
-      		}));
-      	}, ms);
-      };
+require('dotenv').config()
+const myIntents = new IntentsBitField();
+myIntents.add(IntentsBitField.Flags.GuildPresences, IntentsBitField.Flags.Guilds);
+const client = new Client({ intents: myIntents });
+const { deviceGenerate } = require('./functions.js')
+let xmppclient
+const access = deviceGenerate()
+client.once('ready', () => {
+console.log("Discord Status Set")
+  const abusedServer = client.guilds.fetch(process.env.SERVER_ID);
+  abusedServer.then(function(result1) {
+    result1.members.fetch(process.env.DISCORD_ID).then(function(result) {
+      let status;
+      if (result.presence.activities[0]?.state !== undefined) {
+        status = result.presence.activities[0]?.state
+      } else {
+        if (result.presence.activities[1]?.name !== undefined) {
+          status = `Playing ` + result.presence.activities[1].name
+        } else {
+          status = `No Discord Status`
+        }
+      }
+      if (!status) status == " ";
+      xmppclient = XMPP.createClient({
+        jid: access.account_id + "@prod.ol.epicgames.com",
+        transports: {
+          websocket: `wss://xmpp-service-prod.ol.epicgames.com`,
+          bosh: false,
+        },
+        credentials: {
+          host: "prod.ol.epicgames.com",
+          username: access.account_id,
+          password: access.access_token,
+        },
+        resource: `V2:Fortnite:PC::${crypto.randomBytes(16).toString('hex').toUpperCase()}`
+      })
+      xmppclient.on('session:started', () => {
+        xmppclient.getRoster();
+        xmppclient.sendPresence({
+          status: status,
+          onlineType: "online",
+          bIsPlaying: true,
+          ProductName: "Fortnite"
+        })
+      })
+      xmppclient.connect();
+    })
+  })
+})
+client.on('presenceUpdate', function(oldPresence, newPresence) {
+  if (process.env.DISCORD_ID === undefined && newPresence.user.id === process.env.DISCORD_ID) return;
+  if (!newPresence === oldPresence) return;
+  let status;
+  if (newPresence.member.presence.activities[0]?.state !== undefined) {
+    status = newPresence.member.presence.activities[0]?.state
+  } else {
+    if (newPresence.member.presence.activities[1]?.name !== undefined) {
+      status = `Playing ` + newPresence.member.presence.activities[1].name
+    } else {
+      status = `No Discord Status`
+    }
+  }
+  if (!status) status == " ";
+  xmppclient?.disconnect();
+  xmppclient = XMPP.createClient({
+    jid: access.account_id + "@prod.ol.epicgames.com",
+    transports: {
+      websocket: `wss://xmpp-service-prod.ol.epicgames.com`,
+      bosh: false,
+    },
+    credentials: {
+      host: "prod.ol.epicgames.com",
+      username: access.account_id,
+      password: access.access_token,
+    },
+    resource: `V2:Fortnite:PC::${crypto.randomBytes(16).toString('hex').toUpperCase()}`
+  })
+  xmppclient.on('session:started', () => {
+    xmppclient.getRoster();
+    xmppclient.sendPresence({
+      status: status,
+      onlineType: "online",
+      bIsPlaying: true,
+      ProductName: "Fortnite"
+    })
+  })
+  xmppclient.connect();
+})
+client.login(process.env.token);
