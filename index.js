a3ee2c9a5ede471d5d28e4cbc0aca400cd4a2217
Moh <mohammad4sure@gmail.com>
2022-09-05 22:23:17 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const { Client, IntentsBitField } = require('discord.js');
+const {
+	Client,
+	IntentsBitField
+} = require('discord.js');
 const XMPP = require("stanza")
 const crypto = require("crypto")
 require('dotenv').config()
 require('colors');
 const myIntents = new IntentsBitField();
 myIntents.add(IntentsBitField.Flags.GuildPresences, IntentsBitField.Flags.Guilds);
-const client = new Client({ intents: myIntents });
-const { deviceGenerate } = require('./functions.js')
+const client = new Client({
+	intents: myIntents
+});
+const {
+	deviceGenerate
+} = require('./functions.js')
 async function main() {
-let xmppclient;
+	let xmppclient;
 	try {
 		const access = await deviceGenerate()
 		client.once('ready', () => {
@@ -17,27 +24,27 @@ let xmppclient;
 			abusedServer.then(function(result1) {
 				result1.members.fetch(process.env.DISCORD_ID).then(function(result) {
 					let status;
-					if (result.presence?.activities[0]?.applicationId) {
-						if (result.presence?.activities[0]?.name !== undefined) {
+					if(result.presence ? .activities[0] ? .applicationId) {
+						if(result.presence ? .activities[0] ? .name !== undefined) {
 							status = `Playing ` + result.presence.activities[0].name
 						} else {
 							status = `No Discord Status`
 						}
-					} else if (result.presence?.activities[1]?.applicationId) {
-						if (result.presence?.activities[1]?.name !== undefined) {
+					} else if(result.presence ? .activities[1] ? .applicationId) {
+						if(result.presence ? .activities[1] ? .name !== undefined) {
 							status = `Playing ` + result.presence.activities[1].name
 						} else {
 							status = `No Discord Status`
 						}
 					} else {
-						if (result.presence?.activities[0]?.state) {
-							status = result.presence.activities[0]?.state
+						if(result.presence ? .activities[0] ? .state) {
+							status = result.presence.activities[0] ? .state
 						} else {
 							status = `No Discord Status`
 						}
 					}
 					console.log(`Status: ${status}`.blue.bold)
-					 //Set status using xmpp
+						//Set status using xmpp
 					xmppclient = XMPP.createClient({
 						jid: access.account_id + "@prod.ol.epicgames.com",
 						transports: {
@@ -65,31 +72,31 @@ let xmppclient;
 			})
 		})
 		client.on('presenceUpdate', function(oldPresence, newPresence) {
-			if (newPresence.user.id === process.env.DISCORD_ID) {// for some reason return doesn't wanna work
+			if(newPresence.user.id === process.env.DISCORD_ID) { // for some reason return doesn't wanna work
 				console.log("Status Set".blue.bold)
 				let status;
-				if (newPresence?.activities[0]?.applicationId) {
-					if (newPresence?.activities[0]?.name) {
-						status = `Playing ` + newPresence?.presence?.activities[0].name
+				if(newPresence ? .activities[0] ? .applicationId) {
+					if(newPresence ? .activities[0] ? .name) {
+						status = `Playing ` + newPresence ? .presence ? .activities[0].name
 					} else {
 						status = `No Discord Status`
 					}
-				} else if (newPresence?.activities[1]?.applicationId) {
-					if (newPresence?.activities[1]?.name) {
-						status = `Playing ` + newPresence?.presence?.activities[1].name
+				} else if(newPresence ? .activities[1] ? .applicationId) {
+					if(newPresence ? .activities[1] ? .name) {
+						status = `Playing ` + newPresence ? .presence ? .activities[1].name
 					} else {
 						status = `No Discord Status`
 					}
 				} else {
-					if (newPresence?.activities[0]?.state) {
-						status = newPresence.activities[0]?.state
+					if(newPresence ? .activities[0] ? .state) {
+						status = newPresence.activities[0] ? .state
 					} else {
 						status = `No Discord Status`
 					}
 				}
 				console.log(`Status: ${status}`.blue.bold)
-				// Disconnecting xmpp and sending a new one 
-				xmppclient?.disconnect();
+					// Disconnecting xmpp and sending a new one 
+				xmppclient ? .disconnect();
 				// Set status using xmpp
 				xmppclient = XMPP.createClient({
 					jid: access.account_id + "@prod.ol.epicgames.com",
@@ -116,13 +123,11 @@ let xmppclient;
 				xmppclient.connect();
 			}
 		})
-	} catch (error) {
+	} catch(error) {
 		// Error handler
 		console.log(`${error}`.red.bold)
 		process.exit()
 	}
 }
-
 main()
-
 client.login(process.env.BOT_TOKEN);
