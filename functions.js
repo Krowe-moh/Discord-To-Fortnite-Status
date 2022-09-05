021f6bf7f7cacf7cbbcf722614a53f05fa54e49d
Moh <mohammad4sure@gmail.com>
2022-09-05 22:19:21 +1000

    added comments
    :)

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 const fs = require('fs')
 module.exports = {
 	async deviceGenerate() {
-		if (!fs.existsSync(`./deviceauth.json`)) {
+		if (!fs.existsSync(`./deviceauth.json`)) {// checks if deviceauth.js file exists
 			const input = prompt("Authorization Code ");
 			console.log(input)
 			const response = await axios({
@@ -15,7 +15,7 @@ module.exports = {
 				data: `grant_type=authorization_code&code=${input}`
 			}).catch(function(error) {
 				throw "The Authorization Code You Submitted Is Invalid"
-			})
+			})// generates a access_token
 			const response2 = await axios({
 				method: 'post',
 				url: 'https://account-public-service-prod.ol.epicgames.com/account/api/public/account/' + response.data.account_id + `/deviceAuth`,
@@ -23,7 +23,7 @@ module.exports = {
 					'Authorization': `Bearer ${response.data.access_token}`
 				},
 				data: {}
-			})
+			})// generates a device auth
 			let info;
 			info = {
 				secret: response2.data.secret,
@@ -31,7 +31,7 @@ module.exports = {
 				accountId: response.data.account_id,
 			}
 			let data = JSON.stringify(info);
-			fs.writeFileSync('deviceauth.json', data)
+			fs.writeFileSync('deviceauth.json', data)// creates deviceauth.json
 		} else {
 			let devicedata = fs.readFileSync('deviceauth.json');
 			let info = JSON.parse(devicedata);
@@ -46,7 +46,7 @@ module.exports = {
 			}).catch(function(error) {
 				fs.unlinkSync('deviceauth.json')
 				throw "Please Run The Script Again"
-			})
+			})// uses device auth file to generate a access_token
 			return response.data;
 		}
 	}
