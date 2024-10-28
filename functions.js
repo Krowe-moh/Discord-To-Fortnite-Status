f58aa93ca38e4df2203c50e9fd5fb24e6d782369
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2024-10-28 16:12:41 +1100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,58 +1,59 @@
-const axios = require("axios")
+const axios = require("axios");
 const prompt = require('prompt');
-const fs = require('fs')
+const fs = require('fs');
+const authUrl = 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token';
+const deviceAuthUrl = (accountId) => `https://account-public-service-prod.ol.epicgames.com/account/api/public/account/${accountId}/deviceAuth`;
+const authHeader = {
+    'content-type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic NTIyOWRjZDNhYzM4NDUyMDhiNDk2NjQ5MDkyZjI1MWI6ZTNiZDJkM2UtYmY4Yy00ODU3LTllN2QtZjNkOTQ3ZDIyMGM3'
+};
+
 module.exports = {
-	async deviceGenerate() {
-		if (!fs.existsSync(`./deviceauth.json`)) { // checks if deviceauth.js file exists
-			prompt.start();
-			let { input } = await prompt.get([{
-				name: "input"
-			}]);
-			//const input = prompt("Authorization Code ");
-			console.log(input)
-			const response = await axios({
-					method: 'post',
-					url: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token',
-					headers: {
-						'content-type': 'application/x-www-form-urlencoded',
-						'Authorization': 'Basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=',
-					},
-					data: `grant_type=authorization_code&code=${input}`
-				}).catch(function(error) {
-					throw "The Authorization Code You Submitted Is Invalid"
-				}) // generates a access_token
-			const response2 = await axios({
-					method: 'post',
-					url: 'https://account-public-service-prod.ol.epicgames.com/account/api/public/account/' + response.data.account_id + `/deviceAuth`,
-					headers: {
-						'Authorization': `Bearer ${response.data.access_token}`
-					},
-					data: {}
-				}) // generates a device auth
-			let info;
-			info = {
-				secret: response2.data.secret,
-				deviceid: response2.data.deviceId,
-				accountId: response.data.account_id,
-			}
-			let data = JSON.stringify(info);
-			fs.writeFileSync('deviceauth.json', data) // creates deviceauth.json
-		} else {
-			let devicedata = fs.readFileSync('deviceauth.json');
-			let info = JSON.parse(devicedata);
-			const response = await axios({
-					method: 'post',
-					url: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token',
-					headers: {
-						'content-type': 'application/x-www-form-urlencoded',
-						'Authorization': 'Basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=',
-					},
-					data: `grant_type=device_auth&account_id=${info.accountId}&device_id=${info.deviceid}&secret=${info.secret}`
-				}).catch(function(error) {
-					fs.unlinkSync('deviceauth.json')
-					throw "Please Run The Script Again"
-				}) // uses device auth file to generate a access_token
-			return response.data;
-		}
-	}
+    async deviceGenerate() {
+        if (!fs.existsSync(`./deviceauth.json`)) {
+            return await this.requestAuthorization();
+        } else {
+            return await this.useStoredAuth();
+        }
+    },
+
+    async requestAuthorization() {
+        prompt.start();
+        const { input: authCode } = await prompt.get([{ name: "input", description: "Authorization Code" }]);
+
+        try {
+            const tokenResponse = await axios.post(authUrl, `grant_type=authorization_code&code=${authCode}`, { headers: authHeader });
+            const accountId = tokenResponse.data.account_id;
+
+            const deviceResponse = await axios.post(deviceAuthUrl(accountId), {}, {
+                headers: { 'Authorization': `Bearer ${tokenResponse.data.access_token}` }
+            });
+
+            const deviceInfo = {
+                secret: deviceResponse.data.secret,
+                deviceId: deviceResponse.data.deviceId,
+                accountId
+            };
+
+            fs.writeFileSync('deviceauth.json', JSON.stringify(deviceInfo));
+            return deviceInfo;
+
+        } catch (error) {
+            throw new Error("Invalid Authorization Code. Please try again.");
+        }
+    },
+
+    async useStoredAuth() {
+        const deviceData = JSON.parse(fs.readFileSync('deviceauth.json'));
+
+        try {
+            const response = await axios.post(authUrl, `grant_type=device_auth&account_id=${deviceData.accountId}&device_id=${deviceData.deviceId}&secret=${deviceData.secret}`, {
+                headers: authHeader
+            });
+            return response.data;
+        } catch (error) {
+            fs.unlinkSync('deviceauth.json');
+            throw new Error("Stored device auth incorrect. Please run the script again.");
+        }
+    }
 };
